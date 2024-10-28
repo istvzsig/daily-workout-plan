e801de5401591e986c9c3a0284c504d69406a6f4
istvzsig <istvanzsigmond@aol.com>
2024-10-29 01:03:24 +0700

    delete garbals

--- src/backend/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-$jsonData = file_get_contents('../../data/workout_plan.json');
-$workoutPlan = json_decode($jsonData, true);
-$currentDay = date('l'); // 'l' returns the full textual representation of the day (e.g., "Monday")
-$todayWorkout = $workoutPlan['workout_plan'][$currentDay] ?? null;
-require_once("../frontend/index.html")
-?>
