8854f80677eacad6a9970662a7ef6653f134b219
istvzsig <istvanzsigmond@aol.com>
2025-04-07 10:52:07 +0200

    add style link to php ceremony

--- src/script/create_php.sh
+++ src/script/create_php.sh
@@ -12,7 +12,9 @@ create_php() {
 \$currentDay = date('l'); // 'l' returns the full textual representation of the day (e.g., "Monday")
 \$todayWorkout = \$workoutPlan['workout_plan'][\$currentDay] ?? null;
 require_once("../frontend/index.html");
+
 ?>
+<link rel="stylesheet" href="styles.css" />
 EOL
   fi
 }
